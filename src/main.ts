652c0ebcf5cfbcf570406c1ba571eb0a6e0f1788
Nino van Galen <nino.van.galen@hva.nl>
2020-02-27 22:58:34 +0100

    Feat: Updated Swagger sorting algoritm

--- src/main.ts
+++ src/main.ts
@@ -27,22 +27,7 @@ async function bootstrap () {
   SwaggerModule.setup('api', app, document, {
     swaggerOptions: {
       operationsSorter: (a, b) => {
-        const methodsOrder = [
-          'get',
-          'post',
-          'put',
-          'patch',
-          'delete',
-          'options',
-          'trace',
-        ];
-        let result = methodsOrder.indexOf(a.get('method')) - methodsOrder.indexOf(b.get('method'));
-
-        if (result === 0) {
-          result = a.get('path').localeCompare(b.get('path'));
-        }
-
-        return result;
+        return a.get('path').localeCompare(b.get('path'));
       },
     },
   });
