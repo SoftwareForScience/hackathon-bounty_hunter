fb5f6920fd1bb19472fe7b4c32f7d54e364f842c
Martijn Vegter <martijn@martijnvegter.com>
2020-02-27 22:14:58 +0100

    fix: Swagger UI should be sorted alphabetically

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,28 @@ async function bootstrap () {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      operationsSorter: (a, b) => {
+        const methodsOrder = [
+          'get',
+          'post',
+          'put',
+          'patch',
+          'delete',
+          'options',
+          'trace',
+        ];
+        let result = methodsOrder.indexOf(a.get('method')) - methodsOrder.indexOf(b.get('method'));
+
+        if (result === 0) {
+          result = a.get('path').localeCompare(b.get('path'));
+        }
+
+        return result;
+      },
+    },
+  });
 
   await app.startAllMicroservicesAsync();
   await app.listen(port);
